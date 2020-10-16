6a9dbd75a794f512f61695c26114bef6812a74fa
faysal ahmed <hl.faysal1@gmail.com>
2020-10-16 18:32:31 +0600

    runtime added

--- todo/settings.py
+++ todo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1zbo2-ro===29l#t9vzrbu5)hc2a&s#2_h4-gf$mgt_jy^r#ta'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['faysal-todo.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
